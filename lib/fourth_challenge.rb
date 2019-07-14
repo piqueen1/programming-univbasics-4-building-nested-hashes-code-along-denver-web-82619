a2530de8bcbaf9215224ed5a6c9652daf6b1de37
Finn Thye <piqueen@gmail.com>
2019-07-14 22:39:37 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,14 +6,14 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         {
-          :name => ,
-          :age => ,
-          :attitude =>
+          :name => "Benvolio",
+          :age => "17",
+          :attitude => "worried"
         },
         {
-          :name => ,
-          :age => ,
-          :attitude =>
+          :name => "Mercutio",
+          :age => "18",
+          :attitude => "hot-headed"
         }
       ]
    }, 
@@ -23,14 +23,14 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         {
-          :name => ,
-          :age => ,
-          :attitude =>
+          :name => "Steven",
+          :age => "30",
+          :attitude => "confused"
         },
         {
-          :name => ,
-          :age => ,
-          :attitude =>
+          :name => "Nurse",
+          :age => "44",
+          :attitude => "worried"
         }
       ]
    }
